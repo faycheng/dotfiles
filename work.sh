8e9919ad624cc7823bfb1ef40f8a31effeae2636
程飞 <fay.cheng.cn@gmail.com>
2017-09-09 23:45:07 +0800

    add fuzzy search for checkout work env

--- work.sh
+++ work.sh
@@ -6,8 +6,19 @@ WORKS_DIR=$FILE_DIR/private/works
 
 workon() {
     work=$1
+    if [ -z $work ]; then
+        echo 'work name is empty'
+        echo 'Usage:'
+        echo 'workon WORK_NAME'
+        return 0
+    fi
     if [ -f $WORKS_DIR/$work.sh ]; then
         source $WORKS_DIR/$work.sh
+        return 0
+    fi
+    fuzzy_work=$(ls $WORKS_DIR | grep $work | head -n 1)
+    if [ ! -z $fuzzy_work ]; then
+        source $WORKS_DIR/$fuzzy_work
         return 0
     fi
     echo "Path($WORKS_DIR/$work.sh) not found"
