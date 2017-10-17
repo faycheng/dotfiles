ba4dd238f88f47e47cd8e8a194f22a370eb8213d
程飞 <fay.cheng.cn@gmail.com>
2017-10-17 11:28:50 +0800

    replace ss proxy by blackssl

--- proxy.sh
+++ proxy.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 
+proxy="http://127.0.0.1:1235"
+
 _proxy::http::open(){
-    export http_proxy="127.0.0.1:1087"
-    export https_proxy="127.0.0.1:1087"
+    export http_proxy="$proxy"
+    export https_proxy="$proxy"
 }
 
 _proxy::http::close(){
@@ -21,7 +23,8 @@ proxy(){
         env | grep proxy
         ;;
     *)
-        echo "Usage: proxy COMMAND\n"
+        echo "Usage: proxy COMMAND"
+        echo "Default: $proxy\n"
         echo "Management Commands:"
         printf "%-10s %-30s\n" open "open http proxy"
         printf "%-10s %-30s\n" close "close http proxy"
